80e492ef801e0de3aaaee40ac8cdf608393f7029
Girish Mehta <girishmehta100@gmail.com>
2020-10-19 01:46:01 +0530

    Work in progress for authentication component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { title } from "../../config/global.json";
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  title = title;
 
   constructor() { }
 
